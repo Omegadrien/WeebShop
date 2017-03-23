9613bf800a05847af89a82278f3e7c4b7a8714c3
Omegadrien <omegadrien@gmail.com>
2017-03-23 21:48:10 +0100

    news api implemented

--- back-end/web/router/api/news/index.js
+++ back-end/web/router/api/news/index.js
@@ -4,39 +4,49 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 var language = "FR";
 
-function getNews (infoToReturn) {
-  var request = require('request');
+function filterNews (info) {
+    var length = info.directories.length;
+    var infoFiltered = {"directory": [], "length" : length};
 
-  var options = {
+    for(var index = 0; index < length; index++){
+        var value = {};
+
+        value["name"] = info.directories.directory[index].name;
+        value["bannerUrl"] = info.directories.directory[index].banner_url;
+        value["directoryId"] = info.directories.directory[index].id;
+        value["isFixed"] = info.directories.directory[index].standard;
+        value["isNew"] = info.directories.directory[index].new;
+
+        infoFiltered["directory"].push(value);
+    }
+    return infoFiltered;
+};
+
+function getNews (infoToReturn) {
+    var request = require('request');
+    var options = {
     url: 'https://samurai.ctr.shop.nintendo.net/samurai/ws/' + language +
-                                                  '/directories/?shop_id=1', //url that returns the directories of the home of the eShop 
+                                                  '/directories/?shop_id=1', //url that returns the directories of the home of the eShop
     headers: {
       'Accept': 'application/json'
     }
-  };
+ };
 
 function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
-      var info = JSON.parse(body);
-      infoToReturn(info);
-    }
-  };
+        var info = JSON.parse(body);
+        infoToReturn(info);
+  }
+};
 
   request(options, callback);
 };
 
 router.get('/', function (req, res) {
+
     getNews(function(result) {
-      var test = {"directories":
-                    {"directory":
-                      [{"name":"YO-KAI WATCH™ 2 :\nVersion démo spéciale",
-                      "icon_url":"https://kanzashi-ctr.cdn.nintendo.net/i/729af71d3a7ba8a5d16c5d2fd5688ee924b73c11973636f995922a7fdeed1725.jpg",
-                      "icon_width":128,"icon_height":96,
-                      "banner_url":"https://kanzashi-ctr.cdn.nintendo.net/i/44c398e2feabbc19477db475f747b2cb4662380a9b0d2635a16675d4fc0f38f2.jpg"
-                    }]
-                  }};
-      //res.json(test);
-      res.json(result);
+    res.json(filterNews(result));
+    
   });
 });
 
