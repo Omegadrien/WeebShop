39c1fbba38e4641f2ecc682ee659b6da40ba72cf
Omegadrien <omegadrien@gmail.com>
2017-04-02 11:21:04 +0200

    use https

--- back-end/app.js
+++ back-end/app.js
@@ -2,17 +2,21 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 require('./config');
-
 var app = express();
+var https = require('https');
+var fs = require('fs');
 
-app.use(bodyParser.json());
+var options = {
+  key: fs.readFileSync('./keys/ctr-common-1.key'),
+  cert: fs.readFileSync('./keys/ctr-common-1.crt')
+};
 
 //app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(bodyParser.json());
 app.use("/", require("./web/index.js"));
 
 var port = process.env.PORT || 3000;
 
-app.listen(port, function() {
-  console.log(`App runnning on port: ${port}`);
+https.createServer(options, app).listen(port, function() {
+    console.log(`App runnning on port: ${port}`);
 });
