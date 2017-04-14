21152225c7795b6519c39939dd2787c2bd7f5416
Omegadrien <omegadrien@gmail.com>
2017-04-14 22:39:08 +0200

    add gameList

--- front-end/www/js/SettingsController.js
+++ front-end/www/js/SettingsController.js
@@ -3,10 +3,30 @@ angular.module('starter')
 
     $scope.token = sessionService.get("token");
 
+    $http({
+        url: "/api/user/secret/gameList",
+        method: "GET",
+        headers: {"Content-Type":"application/json", "Authorization":"JWT " + $scope.token},
+        data: {'username':$scope.usernameSelect, 'password':$scope.passwordSelect}
+
+    }).then(function success (response) {
+        $scope.listGames = response.data;
+        console.log($scope.listGames.length);
+
+
+    }, function fail(response) {
+        console.log("fail: " + response.data.message);
+    })
+
     $scope.logoutButtonClicked = function() {
         //logout
         sessionService.destroy("token");
         window.location.reload();
+    }
+
+    $scope.buttonClicked = function (contentId) {
+        console.log("contentId=" + contentId);
+        $location.path('/game/' + contentId);
     }
 
 })
